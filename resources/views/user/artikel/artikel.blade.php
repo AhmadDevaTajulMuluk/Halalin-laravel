a48f615207dbacf4f9f7067f4e88a88f7b1841ff
fadhlyrafi <rafiabang@gmail.com>
2024-06-08 12:54:33 +0700

    fix bug jika tidak ada artikel

--- resources/views/user/artikel/artikel.blade.php
+++ resources/views/user/artikel/artikel.blade.php
@@ -14,16 +14,22 @@
       <div class="artikel-container">
         <h2>Artikel Populer</h2>
           <div class="artikel-box">
-            @foreach ($articles as $article)
-              <article class="isi-artikel">
-                <img src="{{ asset( 'image/' . $article->article_image ) }}" alt="Artikel" />
-                  <div class="content-artikel">
-                    <h3>{{ $article->title }}</h3>
-                    <p>{{ Str::limit($article->content, 150) }}</p>
-                    <a href="{{ route('artikel.show', $article->article_id) }}" class="button">Selengkapnya</a>
-                  </div>
-              </article>
-            @endforeach
+            @if ($articles->isEmpty())
+              <div style="display: flex; justify-content: center; align-items: center; height: 200px;">
+                <p>Belum ada artikel yang tersedia.</p>
+              </div>
+            @else
+                @foreach ($articles as $article)
+                    <article class="isi-artikel">
+                        <img src="{{ asset( 'image/' . $article->article_image ) }}" alt="Artikel" />
+                        <div class="content-artikel">
+                            <h3>{{ $article->title }}</h3>
+                            <p>{{ Str::limit($article->content, 150) }}</p>
+                            <a href="{{ route('artikel.show', $article->article_id) }}" class="button">Selengkapnya</a>
+                        </div>
+                    </article>
+                @endforeach
+            @endif
           </div>
           <div id="navigator">
             <style>
