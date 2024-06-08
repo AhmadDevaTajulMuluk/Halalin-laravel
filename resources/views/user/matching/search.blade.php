969f11420562ad456fbfbca03d53e49c0a59d4c9
Annisa Isnaini <nisaisnaini73@gmail.com>
2024-06-08 16:27:15 +0700

    search none block

--- resources/views/user/matching/search.blade.php
+++ resources/views/user/matching/search.blade.php
@@ -16,34 +16,35 @@
               @csrf
               <label for="searchType">Pilih Tipe Pencarian:</label>
               <select id="searchType" name="searchType">
+                <option value="" selected>-- Pilih Tipe Pencarian --</option>
                 <option value="physical">Fisik</option>
                 <option value="nonPhysical">Non-Fisik</option>
               </select>
         
-              <div id="physicalSearch" class="search" style="display: block;">
+              <div id="physicalSearch" class="search" style="display: none;">
                 <h2>Kriteria Fisik</h2>
                 <label for="skincolor">Warna Kulit:</label>
                 <select id="skincolor" name="skincolor">
                   <option value="" selected disabled>-- Pilih Warna Kulit --</option>
-                  <option value="Putih">Putih</option>
-                  <option value="Putih Kecoklatan">Putih Kecoklatan</option>
-                  <option value="Gelap">Gelap</option>
+                  <option value="Putih" >Putih</option>
+                  <option value="Putih Kecoklatan" >Putih Kecoklatan</option>
+                  <option value="Gelap" >Gelap</option>
                 </select>
                 <label for="haircolor">Warna Rambut:</label>
                 <select id="haircolor" name="haircolor">
                   <option value="" selected disabled>-- Pilih Warna Rambut --</option>
-                  <option value="Coklat">Coklat</option>
-                  <option value="Hitam">Hitam</option>
-                  <option value="Pirang">Pirang</option>
+                  <option value="Coklat" >Coklat</option>
+                  <option value="Hitam" >Hitam</option>
+                  <option value="Pirang" >Pirang</option>
                 </select>
                 <label for="minWeight">Berat Badan (minimal):</label>
-                <input type="text" id="minWeight" name="minWeight" value="0" />
+                <input type="text" id="minWeight" placeholder="Masukkan berat badan minimal" name="minWeight" />
                 <label for="maxWeight">Berat Badan (maksimal):</label>
-                <input type="text" id="maxWeight" name="maxWeight" value="100" />
+                <input type="text" id="maxWeight" placeholder="Masukkan berat badan maksimal" name="maxWeight" />
                 <label for="minHeight">Tinggi Badan (minimal):</label>
-                <input type="text" id="minHeight" name="minHeight" value="0" />
+                <input type="text" id="minHeight" placeholder="Masukkan tinggi badan minimal" name="minHeight" />
                 <label for="maxHeight">Tinggi Badan (maksimal):</label>
-                <input type="text" id="maxHeight" name="maxHeight" value="200" />
+                <input type="text" id="maxHeight" placeholder="Masukkan tinggi badan maksimal" name="maxHeight" />
               </div>
         
               <div id="nonPhysicalSearch" class="search" style="display:none;">
@@ -78,24 +79,24 @@
           <div class="container-result">
             <div id="results" class="card-container">
               @isset($results)
-              @foreach($results as $result)
-              <div class="card">
-                <div style="margin : 1rem">
-                  <h2>{{ $result->fullname }}</h2>
-                  @php
-                      $birthdate = new DateTime($result->birth_date);
-                      $today = new DateTime('today');
-                      $age = $birthdate->diff($today)->y;
-                  @endphp
-                  <p>Umur: {{ $age }}</p>
-                  <p>Tempat Lahir: {{ $result->place_date }}</p>
-                  <p>Pendidikan: {{ $result->last_education }}</p>
-                  <p>Hafalan: {{ $result->quran }}</p>
-                  <p>Motto: {{ $result->motto }}</p>
-                  <p>Alasan Taaruf: {{ $result->reason }}</p>
+                @foreach($results as $result)
+                <div class="card">
+                  <div style="margin : 1rem">
+                    <h2>{{ $result->fullname }}</h2>
+                    @php
+                        $birthdate = new DateTime($result->birth_date);
+                        $today = new DateTime('today');
+                        $age = $birthdate->diff($today)->y;
+                    @endphp
+                    <p>Umur: {{ $age }}</p>
+                    <p>Tempat Lahir: {{ $result->place_date }}</p>
+                    <p>Pendidikan: {{ $result->last_education }}</p>
+                    <p>Hafalan: {{ $result->quran }}</p>
+                    <p>Motto: {{ $result->motto }}</p>
+                    <p>Alasan Taaruf: {{ $result->reason }}</p>
+                  </div>
                 </div>
-              </div>
-              @endforeach
+                @endforeach
               @endisset
             </div>
             <div id="notFound" class="not-found" style="display: none;">Data Tidak Ditemukan</div>
@@ -103,21 +104,21 @@
     </main>
     <x-footer></x-footer>
     <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        const searchType = document.getElementById('searchType');
-        const physicalSearch = document.getElementById('physicalSearch');
-        const nonPhysicalSearch = document.getElementById('nonPhysicalSearch');
-        
-        searchType.addEventListener('change', function() {
-          if (this.value === 'physical') {
-            physicalSearch.style.display = 'block';
-            nonPhysicalSearch.style.display = 'none';
-          } else if (this.value === 'nonPhysical') {
-            physicalSearch.style.display = 'none';
-            nonPhysicalSearch.style.display = 'block';
-          }
+      document.getElementById('searchType').addEventListener('change', function() {
+            const physicalSearch = document.getElementById('physicalSearch');
+            const nonPhysicalSearch = document.getElementById('nonPhysicalSearch');
+            
+            if (this.value === 'physical') {
+                physicalSearch.style.display = 'block';
+                nonPhysicalSearch.style.display = 'none';
+            } else if (this.value === 'nonPhysical') {
+                physicalSearch.style.display = 'none';
+                nonPhysicalSearch.style.display = 'block';
+            } else {
+                physicalSearch.style.display = 'none';
+                nonPhysicalSearch.style.display = 'none';
+            }
         });
-      });
     </script>
     <script type="text/javascript" src="../../assets/js/script.js"></script>
 </body>
