84665d7f6e4814fa63818518f433a31d891d77f0
faishaldaffas <faishaldaffa16@gmail.com>
2024-06-10 06:59:18 +0700

    Fix Tampilan Show Profile saat Matching

--- resources/views/user/matching/show-profile.blade.php
+++ resources/views/user/matching/show-profile.blade.php
@@ -19,20 +19,43 @@
             <div class="card-body">
                 <h1 class="mb-4">CV Lengkap</h1>
                 <div class="show-profile" id="show-card">                   
-                    <h3>Profil Pribadi</h3>
+                    <h3>Profil</h3>
                     <div class="show-img">
                     <img src="{{ $user && $user->image ? asset('image/' . $user->image) : '/assets/images/defaultpic.png' }}" alt="user" />
                     </div>
                     <p><b>Nama Lengkap:</b> {{ $user->fullname }}</p>
                     <p><b>Username:</b> {{ $user->username }}</p>
                     <p><b>Email:</b> {{ $user->email }}</p>
+                    <p><b>Jenis Kelamin:</b> {{ $user->gender }}</p>
                     <p><b>Nomor Telepon:</b> {{ $user->phone_number }}</p>
-                    <p><b>Tempat, Tanggal Lahir:</b> {{ $user->place_date }}, {{ $user->birth_date }}</p>
+                    <p><b>Tempat, Tanggal Lahir:</b> {{ $user->place_date }}, {{ \Carbon\Carbon::parse($user->birth_date)->locale('id')->translatedFormat('d F Y') }}</p>
                     <p><b>Jenis Kelamin:</b> {{ $user->gender }}</p>
                     <p><b>Pekerjaan:</b> {{ $user->job }}</p>
                     <p><b>Pendapatan:</b> {{ $user->salary }}</p>
                     <p><b>Status Pernikahan:</b> {{ $user->married_status }}</p>
                     <p><b>Etnis:</b> {{ $user->ethnic }}</p>
+                </div>
+
+                <div class="show-self" id="show-card">
+                    <h3>Gambaran Diri</h3>
+                    <p><b>Motto:</b> {{ $user->motto }}</p>
+                    <p><b>Life Goals:</b> {{ $user->lifegoals }}</p>
+                    <p><b>Hobi:</b> {{ $user->hobbies }}</p>
+                    <p><b>Hal Favorit:</b> {{ $user->favThings }}</p>
+                    <p><b>Sifat Positif:</b> {{ $user->positiveTraits }}</p>
+                    <p><b>Sifat Negatif:</b> {{ $user->negativeTraits }}</p>
+                    <p><b>Alasan Ta'aruf:</b> {{ $user->taarufReason }}</p>
+                </div>
+
+                <div class="show-physical" id="show-card">
+                    <h3>Informasi Fisik</h3>
+                    <p><b>Warna Kulit:</b> {{ $user->skincolor }}</p>
+                    <p><b>Warna Rambut:</b> {{ $user->haircolor }}</p>
+                    <p><b>Tinggi Badan:</b> {{ $user->height }} cm</p>
+                    <p><b>Berat Badan:</b> {{ $user->weight }} kg</p>
+                    <p><b>Penyakit:</b> {{ $user->illness }}</p>
+                    <p><b>Kecacatan:</b> {{ $user->disability }}</p>
+                    <p><b>Ciri Khusus:</b> {{ $user->uniqueTraits }}</p>
                 </div>
 
                 <div class="show-education" id="show-card">
@@ -43,17 +66,14 @@
                     <p><b>Sekolah Menengah Atas:</b> {{ $user->seniorHighSchool }}</p>
                     <p><b>Sarjana:</b> {{ $user->collegeS1 }}</p>
                     <p><b>Jurusan Sarjana:</b> {{ $user->majorS1 }}</p>
-                    <p><b>Sarjana Muda:</b> {{ $user->collegeS2 }}</p>
-                    <p><b>Jurusan Sarjana Muda:</b> {{ $user->majorS2 }}</p>
+                    <p><b>Magister:</b> {{ $user->collegeS2 }}</p>
+                    <p><b>Jurusan Magister:</b> {{ $user->majorS2 }}</p>
                     <p><b>Doktor:</b> {{ $user->collegeS3 }}</p>
                     <p><b>Jurusan Doktor:</b> {{ $user->majorS3 }}</p>
                 </div>
 
                 <div class="show-family" id="show-card">
-                    <h3>Kelahiran dan Keluarga</h3>
-                    <p><b>Tempat, Tanggal Lahir:</b> {{ $user->place_date }}, {{ $user->birth_date }}</p>
-                    <p><b>Jenis Kelamin:</b> {{ $user->gender }}</p>
-                    <p><b>Nama Ayah:</b> {{ $user->fathers_name }}</p>
+                    <h3>Gambaran Keluarga</h3><p><b>Nama Ayah:</b> {{ $user->fathers_name }}</p>
                     <p><b>Pekerjaan Ayah:</b> {{ $user->fathers_job }}</p>
                     <p><b>Nama Ibu:</b> {{ $user->mothers_name }}</p>
                     <p><b>Pekerjaan Ibu:</b> {{ $user->mothers_job }}</p>
@@ -66,34 +86,12 @@
                     <h3>Statistik Agama</h3>
                     <p><b>Hafalan Al-Quran:</b> {{ $user->quranMemory }}</p>
                     <p><b>Tingkat:</b> {{ $user->level }}</p>
-                    <p><b>Jawaban 1:</b> {{ $user->answer1 }}</p>
-                    <p><b>Jawaban 2:</b> {{ $user->answer2 }}</p>
-                    <p><b>Jawaban 3:</b> {{ $user->answer3 }}</p>
-                    <p><b>Jawaban 4:</b> {{ $user->answer4 }}</p>
-                    <p><b>Jawaban 5:</b> {{ $user->answer5 }}</p>
-                    <p><b>Jawaban 6:</b> {{ $user->answer6 }}</p>
-                </div>
-                
-                <div class="show-physical" id="show-card">
-                    <h3>Informasi Fisik</h3>
-                    <p><b>Warna Kulit:</b> {{ $user->skincolor }}</p>
-                    <p><b>Warna Rambut:</b> {{ $user->haircolor }}</p>
-                    <p><b>Tinggi Badan:</b> {{ $user->height }} cm</p>
-                    <p><b>Berat Badan:</b> {{ $user->weight }} kg</p>
-                    <p><b>Penyakit:</b> {{ $user->illness }}</p>
-                    <p><b>Kecacatan:</b> {{ $user->disability }}</p>
-                    <p><b>Ciri Khusus:</b> {{ $user->uniqueTraits }}</p>
-                </div>
-
-                <div class="show-self" id="show-card">
-                    <h3>Aplikasi Diri</h3>
-                    <p><b>Motto:</b> {{ $user->motto }}</p>
-                    <p><b>Life Goals:</b> {{ $user->lifegoals }}</p>
-                    <p><b>Hobi:</b> {{ $user->hobbies }}</p>
-                    <p><b>Hal Favorit:</b> {{ $user->favThings }}</p>
-                    <p><b>Sifat Positif:</b> {{ $user->positiveTraits }}</p>
-                    <p><b>Sifat Negatif:</b> {{ $user->negativeTraits }}</p>
-                    <p><b>Alasan Ta'aruf:</b> {{ $user->taarufReason }}</p>
+                    <p><b>Kajian ustad yang diikuti:</b> <br> {{ $user->answer1 }}</p>
+                    <p><b>Apa pemahaman Anda tentang akhirat dan persiapannya dalam ajaran Islam?:</b> <br> {{ $user->answer2 }}</p>
+                    <p><b>Apa yang Anda ketahui tentang proses pencarian pasangan hidup (taaruf) dalam Islam dan bagaimana Anda mempersiapkan diri untuk itu?:</b> <br> {{ $user->answer3 }}</p>
+                    <p><b>Bagaimana Anda memahami konsep mahar (maskawin) dalam Islam dan bagaimana Anda menentukan nilai atau jenis mahar yang sesuai?:</b> <br> {{ $user->answer4 }}</p>
+                    <p><b>Bagaimana Anda mengatasi konflik antara kebutuhan untuk memenuhi tuntutan dunia modern dengan menjalankan ajaran agama dalam kehidupan sehari-hari?:</b> <br> {{ $user->answer5 }}</p>
+                    <p><b>Bagaimana Anda memandang pentingnya kesetiaan dalam pernikahan dalam Islam, terutama dalam konteks modern yang penuh dengan godaan dan tantangan?:</b> <br> {{ $user->answer6 }}</p>
                 </div>
 
             </div>
