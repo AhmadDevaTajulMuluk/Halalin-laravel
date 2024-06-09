6d3ec14c3756737231967d471b362d131d7640a0
ahmaddevatajulm <ahmaddevatajulmuluk@gmail.com>
2024-06-10 04:11:31 +0700

    sidebar admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,18 +9,18 @@
     </div>
     <ul class="sidebar-nav">
         <li class="sidebar-item">
-            <a href="#" class="sidebar-link">
+            <a href="/admin/dashboard-admin" class="sidebar-link">
                 <i class="lni lni-user"></i>
-                <span>Profile</span>
+                <span>Admin</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        {{-- <li class="sidebar-item">
             <a href="#" class="sidebar-link">
                 <i class="lni lni-agenda"></i>
                 <span>Task</span>
             </a>
-        </li>
-        <li class="sidebar-item">
+        </li> --}}
+        {{-- <li class="sidebar-item">
             <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
                 data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
                 <i class="lni lni-protection"></i>
@@ -34,42 +34,42 @@
                     <a href="#" class="sidebar-link">Register</a>
                 </li>
             </ul>
-        </li>
+        </li> --}}
         <li class="sidebar-item">
             <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
                 data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
                 <i class="lni lni-layout"></i>
-                <span>Multi Level</span>
+                <span>Tabel</span>
             </a>
             <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                 <li class="sidebar-item">
                     <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse"
                         data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
-                        Two Links
+                        Tabel User
                     </a>
                     <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">Link 1</a>
+                            <a href="#" class="sidebar-link">User</a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">Link 2</a>
+                            <a href="#" class="sidebar-link">Ustadz</a>
                         </li>
                     </ul>
                 </li>
             </ul>
         </li>
-        <li class="sidebar-item">
+        {{-- <li class="sidebar-item">
             <a href="#" class="sidebar-link">
                 <i class="lni lni-popup"></i>
                 <span>Notification</span>
             </a>
-        </li>
-        <li class="sidebar-item">
+        </li> --}}
+        {{-- <li class="sidebar-item">
             <a href="#" class="sidebar-link">
                 <i class="lni lni-cog"></i>
                 <span>Setting</span>
             </a>
-        </li>
+        </li> --}}
     </ul>
     <div class="sidebar-footer">
         <a class="sidebar-link" href="{{ route('admin.logout') }}"><i class="lni lni-exit"></i><span>Logout</span></a>
