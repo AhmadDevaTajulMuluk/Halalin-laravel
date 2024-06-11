a0a9401a58a71ff6dd41264822541b9ff01f10d9
fadhlyrafi <rafiabang@gmail.com>
2024-06-12 00:54:41 +0700

    fix bug migration

--- database/migrations/2024_06_11_133111_modify_table_relations.php
+++ database/migrations/2024_06_11_133111_modify_table_relations.php
@@ -11,11 +11,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        // Modify the `chats` table to drop the foreign key if it exists and make `hubungan_id` nullable
+        Schema::table('chats', function (Blueprint $table) {
+            if (Schema::hasColumn('chats', 'hubungan_id')) {
+                $table->dropForeign(['hubungan_id']); // Drop the foreign key if it exists
+                $table->dropColumn('hubungan_id')->nullable()->change();
+            }
+        });
+
+        // Modify the `relations` table
         Schema::table('relations', function (Blueprint $table) {
             $table->id('hubungan_id')->autoIncrement()->change();
             $table->foreignId('ustadz_id')->nullable()->change();
-            $table->boolean('start')->default(false);
+            $table->boolean('start')->default(false)->after('ustadz_id'); // Adding `start` column after `ustadz_id`
+        });
+
+        // Add `hubungan_id` back to the `chats` table and make it nullable
+        Schema::table('chats', function (Blueprint $table) {
+            $table->foreignId('hubungan_id')->nullable()->constrained('relations')->nullOnDelete();
         });
     }
 
