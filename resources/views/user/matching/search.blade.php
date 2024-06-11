c70253a3476c07b39cf58a33b77bf3ce7fbfe97c
Muhammad Rifky Janzani <rjanzani0350@gmail.com>
2024-06-11 22:25:28 +0700

    detail tipis

--- resources/views/user/matching/search.blade.php
+++ resources/views/user/matching/search.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mencari Pasangan</title>
+    <link rel="icon" type="image/jpg" href="../../assets/images/halalin-icon.png">
     <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 
